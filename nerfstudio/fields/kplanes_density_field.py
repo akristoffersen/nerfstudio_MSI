d1d70de19882eb632121c31417cde427d244b9de
gmeanti <giacomo.meanti@gmail.com>
2023-03-07 15:16:11 +0100

    Cleanup kplanes density field, removing unused forward.

--- nerfstudio/fields/kplanes_density_field.py
+++ nerfstudio/fields/kplanes_density_field.py
@@ -8,7 +8,6 @@ from torchtyping import TensorType
 from nerfstudio.cameras.rays import RaySamples
 from nerfstudio.data.scene_box import SceneBox
 from nerfstudio.field_components.activations import trunc_exp
-from nerfstudio.field_components.field_heads import FieldHeadNames
 from nerfstudio.field_components.spatial_distortions import SpatialDistortion
 from nerfstudio.fields.base_field import Field
 from nerfstudio.fields.kplanes_field import init_grid_param, interpolate_ms_features
@@ -53,46 +52,33 @@ class KPlanesDensityField(Field):
         )
 
     def get_density(self, ray_samples: RaySamples):
-
+        positions = ray_samples.frustums.get_positions()
         if self.spatial_distortion is not None:
-            positions = ray_samples.frustums.get_positions()
             positions = self.spatial_distortion(positions)
             positions = (positions + 2.0) / 4.0
         else:
-            positions = SceneBox.get_normalized_positions(ray_samples.frustums.get_positions(), self.aabb)
+            positions = SceneBox.get_normalized_positions(positions, self.aabb)
 
-        pts = positions
-        n_rays, n_samples = pts.shape[:2]
+        n_rays, n_samples = positions.shape[:2]
 
         timestamps = ray_samples.times
         if timestamps is not None:
-            # timestamps = timestamps[:, None].expand(-1, n_samples)[..., None]  # [n_rays, n_samples, 1]
-            pts = torch.cat((pts, timestamps), dim=-1)  # [n_rays, n_samples, 4]
+            # Normalize timestamps from [0, 1] to [-1, 1]
+            timestamps = (timestamps * 2) - 1
+            positions = torch.cat((positions, timestamps), dim=-1)  # [n_rays, n_samples, 4]
 
-        pts = pts.reshape(-1, pts.shape[-1])
+        positions = positions.reshape(-1, positions.shape[-1])
 
         features = interpolate_ms_features(
-            pts, ms_grids=[self.grids], grid_dimensions=2, concat_features=False, num_levels=None
+            positions, ms_grids=[self.grids], grid_dimensions=2, concat_features=False, num_levels=None
         )
         density = trunc_exp(
-            self.sigma_net(features).to(pts)
-            # features.to(pts)
+            self.sigma_net(features).to(positions)
         ).view(n_rays, n_samples, 1)
         return density, features
 
-    def get_outputs(self, ray_samples: RaySamples, density_embedding: Optional[TensorType] = None) -> TensorType:
-        return torch.zeros(ray_samples.shape)
-
-    def forward(
-        self,
-        ray_samples: RaySamples,
-        compute_normals: bool = False,
-        mask: Optional[TensorType] = None,
-        bg_color: Optional[TensorType] = None,
-    ):
-        density = self.get_density(ray_samples)
-
-        return {FieldHeadNames.DENSITY: density}
+    def get_outputs(self, ray_samples: RaySamples, density_embedding: Optional[TensorType] = None):
+        return {}
 
     def get_params(self):
         field_params = {k: v for k, v in self.grids.named_parameters(prefix="grids")}
